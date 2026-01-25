fc7a1b93d0bd7f439642eaac13291333f8ffca60
Guru107 <guruak107@gmail.com>
2026-01-25 16:45:52 +0530

    transit type warehouse

--- npd_project_module/tests/__init__.py
+++ npd_project_module/tests/__init__.py
@@ -1,4 +1,19 @@
 # Copyright (c) 2025, Guru107 and contributors
 # For license information, please see license.txt
 
+"""
+Test dependencies declaration
+"""
+import frappe
+def before_tests():
+    """
+    Before tests hook
+    """
+    # create transit warehouse type
+    if not frappe.db.exists("Warehouse Type", "Transit"):
+        frappe.get_doc({
+            "doctype": "Warehouse Type",
+            "warehouse_type": "Transit"
+        }).insert()
+
 global_test_dependencies = ["Warehouse Type", "User", "Company", "Item", "Project Template"]
